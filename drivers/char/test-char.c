dd1ea5046bf352d23511632fdcc9364eebe0ac19
Hai Yang <hai.yang@verisilicon.com>
2023-05-23 19:54:43 +0800

    test-char: update dts

--- drivers/char/test-char.c
+++ drivers/char/test-char.c
@@ -4,12 +4,19 @@
 #include <linux/module.h>
 #include <linux/of.h>
 #include <linux/of_irq.h>
+#include <linux/mfd/syscon.h>
+#include <linux/regmap.h>
 
 static int test_char_probe(struct platform_device *dev)
 {
 	int retval;
 	int irq;
+	u32 dev_id;
 	const char *mailbox_name;
+	struct regmap *ctrl_base;
+	struct resource *res_irq;
+	struct resource *res_mem;
+	int res_index;
 
 	struct device_node *node = dev->dev.of_node;
 
@@ -18,14 +25,47 @@ static int test_char_probe(struct platform_device *dev)
 	retval = of_property_read_string(node, "mailbox", &mailbox_name);
 	if (!retval)
 		printk("test char: mailbox name is %s\n", mailbox_name);
-	else
+	else {
 		printk("test char: mailbox name is not found\n");
+		return 0;
+	}
+
+	retval = of_property_read_u32_index(node, "dev-id", 0, &dev_id);
+	if (retval)
+		printk("test char: cannot get dev-id\n");
+	else
+		printk("test char: dev-id = %d\n", dev_id);
 
 	irq = platform_get_irq(dev, 0);
 	if (irq < 0)
 		printk("test char: No IRQ\n");
 	else
 		printk("test char: IRQ = %d\n", irq);
+
+#if 0
+	if (of_property_read_bool(node, "syscon-spi-cs")) {
+		ctrl_base = syscon_regmap_lookup_by_phandle(node, "syscon-spi-cs");
+		if (IS_ERR(ctrl_base))
+			printk("ctrl_base is error\n");
+		else
+			printk("ctrl_base is successful\n");
+	}
+#endif
+	res_irq = platform_get_resource(dev, IORESOURCE_IRQ, 0);
+	if (!res_irq)
+		printk("test char: get resource No IRQ\n");
+	else
+		printk("test char: get resource IRQ = %lld\n", res_irq->start);
+
+	for (res_index = 0;;res_index++) {
+		res_mem = platform_get_resource(dev, IORESOURCE_MEM, res_index);
+		if (!res_mem) {
+			printk("test char: get resource No mem\n");
+			break;
+		}
+		else
+			printk("test char: get resource mem start = %llx end = %llx\n", res_mem->start, res_mem->end);
+	}
 
 	return 0;
 }
